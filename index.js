039801175e5548fefc4d3b965c0411f4726dfa9e
ahaurw01 <aaron.haurwitz@gmail.com>
2016-02-27 20:41:06 -0500

    choose random card from mtgjson list

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
+var allCards = require('./all-cards');
+
 exports.handler = function (event, context) {
-  context.done(null, 'hello world!');
+  var cardKeys = Object.keys(allCards);
+  var numCards = cardKeys.length;
+  var randomIndex = Math.floor(Math.random() * numCards);
+  var cardKey = cardKeys[randomIndex];
+  var card = allCards[cardKey];
+  console.log(card);
+  context.done(null, card);
 };
